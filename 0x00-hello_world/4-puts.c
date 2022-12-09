e839f2470e0d85420949029f71992b551b40ea2b
Cassie01Cares <blesift@gmail.com>
2022-12-08 20:10:23 -0800

    size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -18,24 +18,4 @@ int main(void)
 
 			return (0);
 
-}#include <stdio.h>
-
-/**
- *
- *   * main - Entry
- *
- *     * Return: Always 0
- *
- *       */
-
-
-
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
 }
