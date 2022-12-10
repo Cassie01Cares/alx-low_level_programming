16a6da6396c74b320cdc1f053e44c36349dab79d
Cassie01Cares <blesift@gmail.com>
2022-12-10 11:25:44 -0800

    corrections2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 /**
- *  * main - The entry point of all C program
- *   * @void: Doesn't take any parameters
- *    *
- *     * Return: It returns zero upon successful run of program
- *      */
-
+ * main - prints the size of various types
+ *
+ * Return: Returns 0 if program exits properly
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+	return (0);
 }
