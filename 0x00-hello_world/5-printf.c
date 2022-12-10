2f34df46b3a1253c3da31e821c5d812ebbcd2d02
Cassie01Cares <blesift@gmail.com>
2022-12-10 11:23:40 -0800

    corrections1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 /**
- *  * main - The entry point of all C program
- *   * @void: Doesn't take any parameters
- *    *
- *     * Return: It returns zero upon successful run of program
- *      */
+  * main - Entry
+  * Return: Always 0
+*/
 
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+
 }
